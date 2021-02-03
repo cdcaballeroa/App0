4d08b26757e7d08dff63a3fe0c3e6ca9c2619f99
David <cdcaballeroa@unal.edu.co>
2021-02-03 15:32:14 -0500

    fixing bugs and adding bulma scripts and css

--- project/__init__.py
+++ project/__init__.py
@@ -13,7 +13,6 @@ def create_app():
     app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
     app.config["SQLALCHEMY_BINDS"] = {"events": "sqlite:///events.sqlite"}
-    app.config["SERVER_NAME"] = '0.0.0.0:8080'
 
     usersDB.init_app(app)
 
